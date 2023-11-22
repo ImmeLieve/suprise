045218610c858139b0b0058a00451c8ce2408f7e
ImmeLieve <79265530+ImmeLieve@users.noreply.github.com>
2023-11-22 15:33:07 +0000

    added poem

--- Hello.py
+++ Hello.py
@@ -24,46 +24,46 @@ def run():
         page_icon="🎁",
     )
 
-    st.write("# Surprise Mama! 🎅🎁")
+    st.write("# Een brief van de hulp pieten 🎅🎁")
 
     st.sidebar.markdown("Selecteer je cursus.")
     st.markdown('''
-    Lieve mama,
+    *Lieve Hanneke,*
 
-De hulp piet hoorde dat je de laatste tijd veel rust neemt,
-Dat vonden de sint en zijn pieten wel wat vreemd.
-Je staat namelijk niet bekend als een zen persoon.
-Hun baasje druk bezig zien zijn vonden Jut en Aug inmiddels wel gewoon.
+    De hulp piet hoorde dat je de laatste tijd veel rust neemt, \\
+    Dat vonden de sint en zijn pieten wel wat vreemd. \\
+    Je staat namelijk niet bekend als een zen persoon. \\
+    Hun baasje druk bezig zien zijn vonden Jut en Aug inmiddels wel gewoon.
 
-Nu is er altijd wel iemand thuis om hun aandacht te geven,
-Ze hebben al doei gezegd tegen hun rustige leven.
-Om het het nog erger te maken voor hen,
-wordt het huis nu ook verbouwd… daar gaat hun zen….
+    Nu is er altijd wel iemand thuis om hun aandacht te geven,\\
+    Ze hebben inmiddels al gedag gezegd tegen hun rustige leven.\\
+    Om het het nog erger te maken voor hen,\\
+    wordt het huis nu ook verbouwd… daar gaat hun zen…
 
-Gelukkig helpen ze graag met de keuzes die gemaakt moeten worden
-Ze maken van jouw creatieve chaos een orde.
-Wat ze helaas iets te ver gaat,
-Is het geven van digitale raad.
+    Gelukkig helpen ze graag met de keuzes die gemaakt moeten worden \\
+    Ze maken van jouw creatieve chaos een orde.\\
+    Wat ze helaas iets te ver gaat,\\
+    Is het geven van digitale raad.
 
-Whatsappjes, mailen en belletjes vinden ze maar raar,
-Waarom maken baasjes hun leven vrijwillig zo zwaar?
-Daarom hebben ze de pieten ingelicht,
-want elke keer als je begint over een appje klappen ze dicht.
+    Whatsappjes, mailen en belletjes vinden ze maar raar,\\
+    Waarom maken baasjes hun leven vrijwillig zo zwaar?\\
+    Daarom hebben ze de pieten ingelicht,\\
+    want elke keer als je begint over een appje klappen ze dicht.
 
-De pieten hebben lang naar het probleem gekeken,
-Maar ze snapte er ook helemaal niks van en zijn toen bij de IT piet staan smeken.
-Die keek naar het probleem, maar zag er geen…
-Maar blijkbaar was hij daarin alleen.
+    De pieten hebben lang naar het probleem gekeken,\\
+    Maar ze snapte er ook helemaal niks van en zijn toen bij de IT piet staan smeken.\\
+    Die keek naar het probleem, maar zag er geen…\\
+    Maar blijkbaar was hij daarin alleen.
 
-De IT piet ging daarom dus aan de slag,
-want hij heeft maar zo veel uren in de dag.
-Een digitale tool lijkt de uitkomst,
-Die gebruikt kan worden door iemand zonder digitale afkomst.
+    De IT piet ging daarom dus aan de slag,\\
+    Want hij heeft maar zo veel uren in de dag.\\
+    Een digitale tool lijkt de uitkomst,\\
+    Die gebruikt kan worden door iemand zonder digitale afkomst.
 
-De digitale wereld moet namelijk je leven makkelijker maken,
-zodat jij meer tijd hebt voor stressen over het huis, nieuwe relaties en dekens haken.
-Dus laten we beginnen met het leren
-en jij deze onnodige IT stress kan peren.
+    De digitale wereld moet namelijk je leven makkelijker maken,\\
+    Zodat jij meer tijd hebt voor stressen over het huis, nieuwe relaties en dekens haken.\\
+    Dus laten we beginnen met het leren,\\
+    En jij deze onnodige IT stress kan peren.
 
     Welkom bij de jouw hoog nodige spoedcursus *"Hoe moet ik **zelfstandig** met de computer omgaan?"*
 
