768d0b24d7a803045707581731fddbbcdd65f058
ImmeLieve <79265530+ImmeLieve@users.noreply.github.com>
2023-11-23 13:17:25 +0000

    Quiz en foto toegevoegd

--- pages/1 Hoe maak ik een powerpoint?.py
+++ pages/1 Hoe maak ik een powerpoint?.py
@@ -17,19 +17,39 @@ from typing import Any
 import numpy as np
 
 import streamlit as st
-from streamlit.hello.utils import show_code
-
+from PIL import Image
 
 
 
 st.set_page_config(page_title="Powerpoint", page_icon="💻")
 
 st.markdown("# Powerpoint maken")
-st.sidebar.markdown("**Selecteer je cursus.**")
 st.write(
-    """"""
+    """ Na je welverdiende rust, moet je weer vele powerpoints maken,\\
+    Als kunstdocent bruis je natuurlijk met veel creatieve ideeën.\\
+    Alleen besteed jij je tijd soms liever aan andere zaken,\\
+    Het is daarom fijn dat de *designer* van powerpoint mooie ontwerpen kan displayen.
+
+    Oh nee, helaas deze keer niet. \\
+    Maar de tijd tikt helaas wel door... \\
+    Teken een mooiere design voor de slide die je hieronder ziet. \\
+    Doe dat op papier, je krijgt hier 2 minuten voor.
+
+"""
 )
+st.sidebar.markdown("**Selecteer je cursus.**")
+image = Image.open('/workspaces/suprise/lege_powerpoint.png')
+st.image(image)
+on = st.toggle('*Activeer alleen als de powerpoint is goedgekeurd*')
+
+if on:
+    
+    st.write("""
+            Wat ziet de presentatie er mooi uit! \\
+            Je vraagt je natuurlijk nu af wat de volgende stap is.\\
+            Dat is natuurlijk een leuke quiz!! \\
+            Die hopelijk je grote kennis aanduid...
+             """)
 
 
-
-#show_code(animation_demo)
+    
