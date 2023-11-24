d3dc3d0f831127ce3c5845c42026644aeb469766
ImmeLieve <79265530+ImmeLieve@users.noreply.github.com>
2023-11-24 08:33:29 +0000

    verbeteringen

--- quiz_data.py
+++ quiz_data.py
@@ -3,20 +3,20 @@ import streamlit as st
 quiz_data = [
     {"question": "1. What is the purpose of pile probing?",
 "choices": [
-"a) To save money",
-"b) To check for potential voids in proposed pile location",
-"c) To check the conditions of the surrounding soil",
-"d) To ensure they have been installed correctly"
+"To save money",
+"To check for potential voids in proposed pile location",
+"To check the conditions of the surrounding soil",
+"To ensure they have been installed correctly"
 ],
-"answer": "b) To check for potential voids in proposed pile location"
+"answer": "To check for potential voids in proposed pile location"
 },
 {
 "question": "2. How does a pile integrity test work?",
 "choices": [
-"a) Through electromagnetic waves",
-"b) By breaking a small piece of the concrete poured",
-"c) Through engineering calculations",
-"d) Sending stress waves through the pile using a hand-held device or sensor"
+"Through electromagnetic waves",
+"By breaking a small piece of the concrete poured",
+"Through engineering calculations",
+"Sending stress waves through the pile using a hand-held device or sensor"
 ],
 "answer": "d) Sending stress waves through the pile using a hand-held device or sensor"
 }
